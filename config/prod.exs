710c633c73bf3d074cd73ea67e4244f18be7c356
FalconPilot <fpilot.github@gmail.com>
2017-04-16 18:33:34 +0200

    fixed config file

--- config/prod.exs
+++ config/prod.exs
@@ -13,12 +13,13 @@ use Mix.Config
 # which you typically run after static files are built.
 config :webccg, Webccg.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "elixir-webccg.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :webccg, HelloPhoenix.Repo,
+config :webccg, Webccg.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
